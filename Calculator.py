5cb659c40fc98557da0c3b37d07616aff3405a15
Sasha Tarasov <19aleksandrtarasov@gmail.com>
2025-06-10 18:27:06 +0300

    I upgrade conclusian responses and  added try

--- Calculator.py
+++ Calculator.py
@@ -1,13 +1,16 @@
-number1 = int(input('Введите первое число:'))
-number2 = int(input('Введите второе число:'))
-result = int(input('1.Сложение \n2.Вычитание \n3.Умножение \n4.Деление \nВыберите одно из выше перечисленных действий:'))
-if result == 1:
-    print(number1 + number2)
-elif result == 2:
-    print(number1 - number2)
-elif result == 3:
-    print(number1 * number2)
-elif result == 4:
-    print(number1 / number2)
-else:
-    print('Ошибка')
+try:
+    number1 = int(input('Введите первое число:'))
+    number2 = int(input('Введите второе число:'))
+    result = int(input('1.Сложение \n2.Вычитание \n3.Умножение \n4.Деление \nВыберите одно из выше перечисленных действий:'))
+    if result == 1:
+        print(f"Ответ:{number1 + number2}")
+    elif result == 2:
+        print(f"Ответ:{number1 - number2}")
+    elif result == 3:
+        print(f"Ответ:{number1 * number2}")
+    elif result == 4:
+        print(f"Ответ:{number1 / number2}")
+    else:
+        print('Ошибка')
+except ValueError:
+    print('Вы напутали что то с числами')
